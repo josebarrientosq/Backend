3494c35d1ecc97144f06a33c40b2b2c49e461510
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-03-27 22:57:13 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  require('libreria.php');
+//$resultado = $_POST['vals']; 
+//echo $resultado;
+//  if(empty($_POST['Id']))
+  //	echo "vacio";
+ // else
+ // 	echo $_POST['Id'];
+//echo"valor".$resp;
+
+echo json_encode(getInfobycity("New York"));
+
+
+
+?>
