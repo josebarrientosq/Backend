bfe3d725e424002198cdedb5d0cf9e9047cd148f
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-04-10 21:42:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-  require('libreria.php');
-//$resultado = $_POST['vals']; 
-//echo $resultado;
-//  if(empty($_POST['Id']))
-  //	echo "vacio";
- // else
- // 	echo $_POST['Id'];
-//echo"valor".$resp;
-
-echo json_encode(getInfobycity("New York"));
+require('libreria.php');
+echo json_encode(getInfoTodos());
 
 
 
