dea837916bf454ce35f62998babfa7d648530bc6
josebarrientosq <35469345+josebarrientosq@users.noreply.github.com>
2018-04-10 21:40:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 /*
   Creación de una función personalizada para jQuery que detecta cuando se detiene el scroll en la página
 */
+
+var buscarciudad=true
+var buscartipo=true
+
+function formato(obj,key){
+  var tarjeta="<div class='row resultados'>"
+  tarjeta+="<div class='col s12 m12'>"
+  //tarjeta+="<h2 class'header'>Horizontal Card</h2>"
+  tarjeta+="<div class='card horizontal'>"
+  tarjeta+="<div class='row'>"
+  tarjeta+="<div class='card-image col s6 m6'>"
+  tarjeta+="<img src='home.jpg'>"
+  tarjeta+="</div>"
+  tarjeta+="<div class='card-stacked col s6 m6'>"
+  tarjeta+="<div class='card-content'>"
+  tarjeta+="<p>Direccion : "+obj[key].Direccion 
+  tarjeta+="<p>Ciudad : "+obj[key].Ciudad
+  tarjeta+="<p>Telefono : "+obj[key].Telefono
+  tarjeta+="<p>Codigo postal : "+obj[key].Codigo_Postal
+  tarjeta+="<p>Tipo : "+obj[key].Tipo
+  tarjeta+="<p>Precio : "+obj[key].Precio
+  tarjeta+="</div>"
+  tarjeta+="<div class='card-action'>"
+  tarjeta+="<a href='#'>ver mas</a>"
+  tarjeta+="</div></div></div></div></div></div>"
+
+  return tarjeta
+
+}
+
 $.fn.scrollEnd = function(callback, timeout) {
   $(this).scroll(function(){
     var $this = $(this);
@@ -72,7 +102,8 @@ $( "#mostrarTodos" ).click(function() {
     
     success: function(data){
       var obj = JSON.parse(data)
-    //  $(".colContenido").append(obj)
+    
+    $(".resultados").remove() 
        
     for (var key in obj){
      // console.log(obj[key].Id)
@@ -94,42 +125,121 @@ $( "#mostrarTodos" ).click(function() {
 
   });
 
-function formato(obj,key){
-  var tarjeta="<div class='row'>"
-  tarjeta+="<div class='col s12 m12'>"
-  //tarjeta+="<h2 class'header'>Horizontal Card</h2>"
-  tarjeta+="<div class='card horizontal'>"
-  tarjeta+="<div class='row'>"
-  tarjeta+="<div class='card-image col s6 m6'>"
-  tarjeta+="<img src='home.jpg'>"
-  tarjeta+="</div>"
-  tarjeta+="<div class='card-stacked col s6 m6'>"
-  tarjeta+="<div class='card-content'>"
-  tarjeta+="<p>Direccion : "+obj[key].Direccion 
-  tarjeta+="<p>Ciudad : "+obj[key].Ciudad
-  tarjeta+="<p>Telefono : "+obj[key].Telefono
-  tarjeta+="<p>Codigo postal : "+obj[key].Codigo_Postal
-  tarjeta+="<p>Tipo : "+obj[key].Tipo
-  tarjeta+="<p>Precio : "+obj[key].Precio
-  tarjeta+="</div>"
-  tarjeta+="<div class='card-action'>"
-  tarjeta+="<a href='#'>ver mas</a>"
-  tarjeta+="</div></div></div></div></div></div>"
 
-  return tarjeta
-
-}
 
 
   
 });
 
 
+$(".filtroCiudad").click(function() {
+ if(buscarciudad){
+
+  $.ajax({
+
+    url: './buscarciudad.php',
+    data: {vals : "5"},
+    type: 'post',
+    
+    success: function(data){
+      var obj = JSON.parse(data)
+
+            
+    for (var key in obj)
+
+      $("#selectCiudad").append("<option value='"+obj[key]+"'>"+obj[key]+"</option>")
+ 
+   
+     buscarciudad=false
+
+      
+    },
+    error: function(){
+      alert("error al enviar los datos");
+    }
+   
+
+  });
+
+  }
+});
 
 
+$(".filtroTipo").click(function() {
+ if(buscartipo){
+
+  $.ajax({
+
+    url: './buscartipo.php',
+    data: {vals : "5"},
+    type: 'post',
+    
+    success: function(data){
+      var obj = JSON.parse(data)
+
+            
+    for (var key in obj)
 
 
+      $("#selectTipo").append("<option value='"+obj[key]+"'>"+obj[key]+"</option>")
+
+    
+     buscartipo=false
+
+      
+    },
+    error: function(){
+      alert("error al enviar los datos");
+    }
+   
+
+  });
+
+  }
+});
 
 
+$("#submitButton").click(function() {
+
+  var rango = $("#rangoPrecio").val().split(";")
+
+ 
+
+  $.ajax({
+
+    url: './buscador.php',
+    data: {
+
+    "Ciudad" :  $("#selectCiudad").val(),
+    "Tipo" :  $("#selectTipo").val(),
+    "Precioini": rango[0],
+    "Preciofin": rango[1]
+
+     },
+    type: 'post',
+    
+    success: function(data){
+      var obj = JSON.parse(data)
+     console.log(obj.length)
 
 
+    $(".resultados").remove()
+    if(obj.length==0)
+      $(".colContenido").append("<div class='row resultados'>No se encontraron resultados</div>")
+    else  
+     $(".colContenido").append("<div class='row resultados'>Se encontraron "+obj.length +" resultados</div>")         
+   for (var key in obj){
+
+      $(".colContenido").append(formato(obj,key))
+   }
+  
+    },
+    error: function(){
+      alert("error al enviar los datos");
+    }
+   
+
+  });
+
+  
+});
